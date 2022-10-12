70b8238d68b57ea994ebf55444e43a2c4328b361
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-12 14:23:05 +0200

    Username

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-let t = [1,2,3];
-t = t.filter(e => e > 2);
-
+const email = document.getElementById('email1');
+const reemail = document.getElementById('email2');
+const password = document.getElementById('password');
+var hiba1 :number = 0;
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("szoveg")!.addEventListener('input', (e : Event) => {
-        /*let elem = document.getElementById("szoveg") as HTMLInputElement;
-        document.body.style.backgroundColor = elem.value;*/
-        let elem = e.currentTarget as HTMLInputElement;
-        document.body.style.backgroundColor = elem.value;
+    document.getElementById("username")!.addEventListener("change", () => {
+        const username = (document.getElementById('username') as HTMLInputElement).value;
+        const conditions = ["&","=","_","'","-","+",",","<",">",".."];
+        if (username.length > 5 && username.length < 31){
+            if (!conditions.some(el => username.includes(el))){
+                hiba1 = 0;
+            } else {
+                hiba1 = 2;
+            }
+        } else {
+            hiba1 = 1;
+        }
+    });
+    document.getElementById("submit")!.addEventListener("click", () => {
+        if (hiba1 === 0) {
+
+        } else if (hiba1 === 1) {
+            alert("Felhasználónévnek 6-30 karakter hoszúnak kell lennie!");
+        } else if (hiba1 === 2){
+            alert("Felhasználónév nem megfelelő karaktert tartalmaz")
+        }
     });
 });
