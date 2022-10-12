29564ca9f526e8808c97d3ddba3eb858129a6d8c
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-12 14:39:10 +0200

    Email

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 "use strict";
-const email = document.getElementById('email1');
-const reemail = document.getElementById('email2');
 const password = document.getElementById('password');
 var hiba1 = 0;
+var hiba2 = 0;
+var hiba3 = 0;
+var hiba4 = 0;
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("username").addEventListener("change", () => {
         const username = document.getElementById('username').value;
@@ -19,14 +20,52 @@ document.addEventListener("DOMContentLoaded", () => {
             hiba1 = 1;
         }
     });
+    document.getElementById("email1").addEventListener("change", () => {
+        const email = document.getElementById('email1').value;
+        const validateEmail = (email) => {
+            return String(email)
+                .toLowerCase()
+                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        };
+        if ((validateEmail(email))) {
+            hiba2 = 0;
+        }
+        else {
+            hiba2 = 1;
+        }
+    });
+    document.getElementById("email2").addEventListener("change", () => {
+        const email = document.getElementById('email1').value;
+        const reemail = document.getElementById('email2').value;
+        if (email != reemail) {
+            hiba3 = 1;
+        }
+        else {
+            hiba3 = 0;
+        }
+    });
     document.getElementById("submit").addEventListener("click", () => {
         if (hiba1 === 0) {
+            if (hiba2 === 0) {
+                if (hiba3 === 0) {
+                    if (hiba4 === 0) {
+                    }
+                    else {
+                    }
+                }
+                else {
+                    alert("Nem egyezik a két email!");
+                }
+            }
+            else {
+                alert("Email nem megfelelő!");
+            }
         }
         else if (hiba1 === 1) {
             alert("Felhasználónévnek 6-30 karakter hoszúnak kell lennie!");
         }
         else if (hiba1 === 2) {
-            alert("Felhasználónév nem megfelelő karaktert tartalmaz");
+            alert("Felhasználónév nem megfelelő karaktert tartalmaz!");
         }
     });
 });
